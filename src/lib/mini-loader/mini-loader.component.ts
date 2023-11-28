42412bb1136790bedb6f272f15f50d51f3a2f6d1
Lluis Simon Argemi <lluis.simon@waynabox.com>
2023-11-28 15:02:08 +0100

    fixed peerDependencies versions

--- src/lib/mini-loader/mini-loader.component.ts
+++ src/lib/mini-loader/mini-loader.component.ts
@@ -1,12 +1,11 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'mini-loader',
-  templateUrl: './mini-loader.component.html',
-  styleUrls: ['./mini-loader.component.scss']
+    selector: 'mini-loader',
+    templateUrl: './mini-loader.component.html',
+    styleUrls: ['./mini-loader.component.scss'],
 })
 export class MiniLoaderComponent {
-  constructor() { }
-  @Input() isVisible: boolean = true;
-  @Input() hasLightTheme: boolean = false;
+    @Input() isVisible = true;
+    @Input() hasLightTheme = false;
 }
